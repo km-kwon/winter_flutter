bcba770ed3f633589a0dae111dd034efd28be754
kyoungmin <lanos5019@ajou.ac.kr>
2023-01-09 23:40:21 +0900

    23-01-09 commit

---
+++ toonflix/lib/widgets/button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final Color bgcolor;
+  final Color textcolor;
+
+  const Button({
+    super.key,
+    required this.text,
+    required this.bgcolor,
+    required this.textcolor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: bgcolor, borderRadius: BorderRadius.circular(45)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 50,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textcolor,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
