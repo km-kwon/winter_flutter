b5256b76966d0af924f4a9818ec4acf3a055cdd3
kyoungmin <lanos5019@ajou.ac.kr>
2023-11-06 21:24:58 +0900

    feat: dropdown menu add

--- Desktop/university/3-2/cap/mogang/mogang/src/components/default/Header.tsx
+++ Desktop/university/3-2/cap/mogang/mogang/src/components/default/Header.tsx
@@ -2,16 +2,153 @@ import React from "react";
 import HeaderMain from "./styled/headerStyle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+
 function Header() {
+  const [isSubMenu1Visible, setSubMenu1Visible] = useState(false);
+  const [isSubMenu2Visible, setSubMenu2Visible] = useState(false);
+  const [isSubMenu3Visible, setSubMenu3Visible] = useState(false);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      const isClickOutside =
+        !event.target.closest(".menu") &&
+        !event.target.closest(".sub-menu1") &&
+        !event.target.closest(".sub-menu2") &&
+        !event.target.closest(".sub-menu3");
+      if (isClickOutside) {
+        setSubMenu1Visible(false);
+        setSubMenu2Visible(false);
+        setSubMenu3Visible(false);
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const menuItems = [
+    {
+      title: "카테고리",
+      url: "/",
+      submenu: [
+        {
+          title: "개발1",
+          url: "/",
+          submenu: [
+            {
+              title: "1의 웹 개발",
+              url: "/",
+              submenu: [
+                { title: "JavaScript", url: "/" },
+                { title: "ReactJS", url: "/" },
+              ],
+            },
+            {
+              title: "앱 개발",
+              url: "/",
+            },
+          ],
+        },
+        {
+          title: "우야호",
+          url: "/",
+        },
+        { title: "개발", url: "/" },
+      ],
+    },
+  ];
+
+  const handleSubMenu1Click = () => {
+    setSubMenu1Visible(!isSubMenu1Visible);
+    setSubMenu2Visible(false);
+    setSubMenu3Visible(false);
+  };
+
+  const handleSubMenu2Click = () => {
+    setSubMenu2Visible(!isSubMenu2Visible);
+    setSubMenu3Visible(false);
+  };
+
   return (
     <HeaderMain>
       <ul className="topleft">
         <li>
           <img className="textImg" src="img/mogangtext.png" alt="모강" />
         </li>
-        <li>카테고리</li>
-        <li>로드맵</li>
-        <li>커뮤니티</li>
+        {menuItems.map((menu, index) => {
+          return (
+            <li
+              style={{
+                height: "4rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "8.5rem",
+                position: "relative",
+              }}
+              key={index}
+              onMouseEnter={handleSubMenu1Click}
+            >
+              <span>{menu.title}</span>
+              {menu.submenu && isSubMenu1Visible && (
+                <div className="sub-menu1">
+                  <div style={{ position: "relative" }}>
+                    {menu.submenu.map((menu, index) => {
+                      return (
+                        <li
+                          className="sub-menu1-list"
+                          key={index}
+                          onClick={handleSubMenu2Click}
+                        >
+                          <span>{menu.title}</span>
+                          {menu.submenu && isSubMenu2Visible && (
+                            <div className="sub-menu2">
+                              <div style={{ position: "relative" }}>
+                                {menu.submenu.map((menu, index) => {
+                                  return (
+                                    <li
+                                      className="sub-menu2-list"
+                                      key={index}
+                                      onClick={() => setSubMenu3Visible(true)}
+                                    >
+                                      <span>{menu.title}</span>
+                                      {menu.submenu && isSubMenu3Visible && (
+                                        <div className="sub-menu3">
+                                          {menu.submenu.map((menu, index) => {
+                                            return (
+                                              <li
+                                                className="sub-menu3-list"
+                                                key={index}
+                                              >
+                                                <a href={menu.url}>
+                                                  {menu.title}
+                                                </a>
+                                              </li>
+                                            );
+                                          })}
+                                        </div>
+                                      )}
+                                    </li>
+                                  );
+                                })}
+                              </div>
+                            </div>
+                          )}
+                        </li>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </li>
+          );
+        })}
+        <li className="menu">비교하기</li>
+        <li className="menu">로드맵</li>
+        <li className="menu">커뮤니티</li>
       </ul>
       <ul className="topRight">
         <li>
--- Desktop/university/3-2/cap/mogang/mogang/src/components/default/styled/headerStyle.tsx
+++ Desktop/university/3-2/cap/mogang/mogang/src/components/default/styled/headerStyle.tsx
@@ -12,7 +12,6 @@ const HeaderMain = styled.div`
     align-items: center;
   }
   li {
-    margin-right: 2rem;
     font-weight: 600;
     font-size: 1.2rem;
   }
@@ -31,6 +30,7 @@ const HeaderMain = styled.div`
     background-color: #f2f2f2;
     border-radius: 10px;
     font-size: 1rem;
+    margin-right: 2rem;
   }
   .searchBar input {
     margin: 0 0 0 0.3rem;
@@ -46,6 +46,7 @@ const HeaderMain = styled.div`
     outline: solid 2px black;
     border-radius: 5px;
     width: 3.5rem;
+    margin-right: 2rem;
   }
   .signBtn {
     display: flex;
@@ -56,6 +57,80 @@ const HeaderMain = styled.div`
     outline: solid 2px #3499fd;
     color: #3499fd;
     border-radius: 5px;
+  }
+  a {
+    text-align: center;
+    text-decoration: none;
+    color: inherit;
+  }
+  .menu {
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 8.5rem;
+    position: relative;
+  }
+  .sub-menu1 {
+    display: flex;
+    flex-direction: column;
+    color: #3499fd;
+    align-items: center;
+    position: absolute;
+    top: 3rem;
+    height: 20rem;
+    width: 100%;
+    background-color: white;
+    border: 0.5px solid gray;
+    border-radius: 0.3rem;
+    box-shadow: 17px 17px 20px -10px black;
+  }
+  .sub-menu1-list {
+    display: flex;
+    margin-top: 1rem;
+    color: gray;
+  }
+  .sub-menu1-list {
+    color: #3499fd;
+  }
+  .sub-menu2 {
+    display: flex;
+    flex-direction: column;
+    color: #3499fd;
+    align-items: center;
+    position: absolute;
+    left: 5.6rem;
+    top: -1px;
+    height: 20rem;
+    width: 8.5rem;
+    background-color: white;
+    border: 0.5px solid gray;
+    border-radius: 0.3rem;
+    box-shadow: 17px 17px 20px -10px black;
+  }
+  .sub-menu2-list {
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+  .sub-menu3 {
+    display: flex;
+    flex-direction: column;
+    color: #3499fd;
+    align-items: center;
+    position: absolute;
+    left: 7.4rem;
+    top: -1px;
+    height: 20rem;
+    width: 8.5rem;
+    background-color: white;
+    border: 0.5px solid gray;
+    border-radius: 0.3rem;
+    box-shadow: 17px 17px 20px -10px black;
+  }
+  .sub-menu3-list {
+    display: flex;
+    margin-top: 1rem;
   }
 `;
 
